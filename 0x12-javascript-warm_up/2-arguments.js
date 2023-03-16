1e0a4cfadf66319fff3f76a0bd24a2733e8d8e8d
Al-amin Ahmed <alaacodes@gmail.com>
2023-03-16 15:32:32 +0000

    Task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+t argLen = process.argv.length;
+
+if (argLen === 3) {
+  console.log('Argument found');
+} else if (argLen > 3) {
+  console.log('Arguments found');
+} else {
+  console.log('No argument');
+}
